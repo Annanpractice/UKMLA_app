bed03139f034c25050ba468d09feffd198c57f3f
Annanpractice <is209@st-andrews.ac.uk>
2026-09-21 20:38:36 +0100

    Bump GPU tester to 0.1.1

--- gpu-tester/app/build.gradle.kts
+++ gpu-tester/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
  namespace="uk.co.ukmla.gputester"; compileSdk=35
- defaultConfig { applicationId="uk.co.ukmla.gputester"; minSdk=28; targetSdk=35; versionCode=1; versionName="0.1.0"; testInstrumentationRunner="androidx.test.runner.AndroidJUnitRunner" }
+ defaultConfig { applicationId="uk.co.ukmla.gputester"; minSdk=28; targetSdk=35; versionCode=2; versionName="0.1.1"; testInstrumentationRunner="androidx.test.runner.AndroidJUnitRunner" }
  if(project.hasProperty("testNoNative")) sourceSets.getByName("main").jniLibs.setSrcDirs(emptyList<String>())
  signingConfigs { getByName("debug") { storeFile=rootProject.file("preview.keystore") } }
  buildTypes { getByName("release") { signingConfig=signingConfigs.getByName("debug") } }
