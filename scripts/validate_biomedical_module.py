fa0c3bfc1d9ba96009663512124c9f4b1f3f3fd6
Annanpractice <is209@st-andrews.ac.uk>
2026-09-25 17:01:29 +0100

    Validate new Foundation guideline cards

--- scripts/validate_biomedical_module.py
+++ scripts/validate_biomedical_module.py
@@ -15,7 +15,7 @@ APP = ROOT / "v2" / "app.html"
 
 EXPECTED_ANATOMY = 139
 EXPECTED_PHYSIOLOGY = 215
-EXPECTED_TOTAL = 867
+EXPECTED_TOTAL = 871
 EXPECTED_TOPICS = 26
 
 ANATOMY_SENTINELS = {
@@ -60,6 +60,10 @@ CURATED_CLINICAL_SENTINELS = {
     "MRI safety screening before scanning",
     "Schedule 2 and 3 controlled-drug prescription requirements",
     "Patient dies on the ward: confirmation, certification and referral",
+    "Oxygen prescribing in the acutely ill adult",
+    "Parkinson’s disease in hospital and nil by mouth",
+    "Rapid tranquillisation is not delirium treatment",
+    "Recognising high-dose antipsychotic therapy",
 }
 
 
